61a50a93e250369b059063b19972b892b8005e4f
seantcanavan@gmail.com <seantcanavan@gmail.com>
2024-01-09 20:51:22 -0600

    try allowing all hosts
    Signed-off-by: seantcanavan@gmail.com <seantcanavan@gmail.com>

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".awsapprunner.com", "127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = [".awsapprunner.com", "127.0.0.1", "localhost"]
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
