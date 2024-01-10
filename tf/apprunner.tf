3266bef8b8ec81c384b280471fb0ef65156868bd
seantcanavan@gmail.com <seantcanavan@gmail.com>
2024-01-09 18:58:20 -0600

    inject secrets back into the environment
    Signed-off-by: seantcanavan@gmail.com <seantcanavan@gmail.com>

--- tf/apprunner.tf
+++ tf/apprunner.tf
@@ -20,9 +20,18 @@ resource "aws_apprunner_service" "django-app-runner" {
         value = "django-${var.stage}" # Replace with your branch name
       }
 
+
       code_configuration {
         configuration_source = "REPOSITORY"
-      }
+        code_configuration_values {
+          runtime_environment_variables = {
+            "DATABASE_HOST" = var.django-psql-db-host
+            "DATABASE_PASS" = var.django-psql-db-pass
+            "DATABASE_USER" = var.django-psql-db-user
+            "SECRET_KEY" = var.django-secret-key
+          }
+          runtime = "python311"
+        }
     }
 
     authentication_configuration {
