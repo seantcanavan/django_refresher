8d07d460d68950c79552a93cb8bbb8d94641ae63
seantcanavan@gmail.com <seantcanavan@gmail.com>
2024-01-09 19:09:25 -0600

    revert changes to environment variable loading
    Signed-off-by: seantcanavan@gmail.com <seantcanavan@gmail.com>

--- tf/apprunner.tf
+++ tf/apprunner.tf
@@ -10,6 +10,11 @@ resource "aws_apprunner_service" "django-app-runner" {
     protocol            = "HTTP"
   }
 
+  instance_configuration {
+    cpu    = "2048"
+    memory = "4096"
+  }
+
   source_configuration {
     auto_deployments_enabled = true
 
@@ -17,30 +22,17 @@ resource "aws_apprunner_service" "django-app-runner" {
       repository_url = "https://github.com/seantcanavan/django_refresher"
       source_code_version {
         type  = "BRANCH"
-        value = "django-${var.stage}" # Replace with your branch name
+        value = "django-${var.stage}"
       }
 
 
       code_configuration {
         configuration_source = "REPOSITORY"
-        code_configuration_values {
-          runtime_environment_variables = {
-            "DATABASE_HOST" = var.django-psql-db-host
-            "DATABASE_PASS" = var.django-psql-db-pass
-            "DATABASE_USER" = var.django-psql-db-user
-            "SECRET_KEY" = var.django-secret-key
-          }
-          runtime = "python311"
-        }
+      }
     }
 
     authentication_configuration {
       connection_arn = "arn:aws:apprunner:us-east-2:205503630745:connection/seantcanavan-github-connection/a6ab0e9b45bb4f5796c6ce8904eddcf3"
     }
-  }
-
-  instance_configuration {
-    cpu    = "2048"
-    memory = "4096"
   }
 }
