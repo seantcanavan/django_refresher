013d2a55f9c97db7e7a8281198d1cf111debf783
seantcanavan@gmail.com <seantcanavan@gmail.com>
2024-01-11 11:17:53 -0600

    run env_test from python3 and pipenv
    Signed-off-by: seantcanavan@gmail.com <seantcanavan@gmail.com>

--- polls/views.py
+++ polls/views.py
@@ -8,3 +8,16 @@ def index(request):
     DATABASE_PASS = os.getenv("DATABASE_PASS")
     DATABASE_HOST = os.getenv("DATABASE_HOST")
     return HttpResponse(f"Hello, world. You're at the polls index. {DATABASE_USER} and {DATABASE_PASS} and {DATABASE_HOST}.")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
