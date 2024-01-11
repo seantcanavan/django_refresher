d44bd95f609d7dfbe0c1eaa061403c992401f870
seantcanavan@gmail.com <seantcanavan@gmail.com>
2024-01-11 09:37:40 -0600

    debug connections to the web
    Signed-off-by: seantcanavan@gmail.com <seantcanavan@gmail.com>

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,10 @@
+import os
+
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index. 12345")
+    DATABASE_USER = os.getenv("DATABASE_USER")
+    DATABASE_PASS = os.getenv("DATABASE_PASS")
+    DATABASE_HOST = os.getenv("DATABASE_HOST")
+    return HttpResponse(f"Hello, world. You're at the polls index. {DATABASE_USER} and {DATABASE_PASS} and {DATABASE_HOST}.")
