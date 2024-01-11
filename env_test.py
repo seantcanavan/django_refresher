7146b5de38874e85981a5a32322643379e3c42de
seantcanavan@gmail.com <seantcanavan@gmail.com>
2024-01-11 13:38:40 -0600

    print everything
    Signed-off-by: seantcanavan@gmail.com <seantcanavan@gmail.com>

--- env_test.py
+++ env_test.py
@@ -1,8 +1,17 @@
 import os
+import sys
 
 DATABASE_HOST = os.getenv("DATABASE_HOST")
 DATABASE_USER = os.getenv("DATABASE_USER")
 DATABASE_PASS = os.getenv("DATABASE_PASS")
+
+print('ENV_TEST-----------------')
+
+if len(sys.argv) > 1:
+    parameter = sys.argv[1]
+    print("The command-line parameter is:", parameter)
+else:
+    print("No command-line parameter provided.")
 
 print("ENV_TEST.PY: printing database creds")
 print(DATABASE_USER, DATABASE_PASS, DATABASE_HOST)
