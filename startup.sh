da6416bdca61e784b409db0ccfcadde2db723432
seantcanavan@gmail.com <seantcanavan@gmail.com>
2024-01-16 14:23:18 -0600

    pring debug output
    Signed-off-by: seantcanavan@gmail.com <seantcanavan@gmail.com>

--- startup.sh
+++ startup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+echo "run python3 manage.py collectstatic"
 pipenv run python3 manage.py collectstatic
+echo "pipenv run python3 manage.py test"
 pipenv run python3 manage.py test
+echo "pipenv run python3 manage.py makemigrations"
 pipenv run python3 manage.py makemigrations
+echo "pipenv run python3 manage.py migrate"
 pipenv run python3 manage.py migrate
+echo "pipenv run gunicorn --workers 2 mysite.wsgi"
 pipenv run gunicorn --workers 2 mysite.wsgi
