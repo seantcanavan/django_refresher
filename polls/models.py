c09296856524f02128883f73c974372fbdd666c5
seantcanavan@gmail.com <seantcanavan@gmail.com>
2024-01-16 14:28:56 -0600

    add integer field
    Signed-off-by: seantcanavan@gmail.com <seantcanavan@gmail.com>

--- polls/models.py
+++ polls/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
-    test_int = models.IntegerField
+    test_int = models.IntegerField(default=0)
 
 
 class Choice(models.Model):
